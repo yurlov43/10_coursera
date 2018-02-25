538e4ec86768f151ee7b2596a78c1f981fa1d9db
yurlov43 <yurlov43@yandex.ru>
2018-02-25 22:28:48 +0300

    Completion of code style before PEP8

--- coursera.py
+++ coursera.py
@@ -19,15 +19,25 @@ def get_course_info(course_link):
     html_page = requests.get(course_link).text
     html_tree = BeautifulSoup(html_page, 'html.parser')
     course_info = []
-    course_info.append(html_tree.find('h1', class_='title display-3-text').getText())
-    course_info.append(html_tree.find('div', class_='rc-Language').getText())
-    course_info.append(html_tree.find('div', class_='startdate rc-StartDateString caption-text').getText())
-    rating = html_tree.find('div', class_='ratings-text headline-2-text')
+    course_info.append(html_tree.find(
+        'h1',
+        class_='title display-3-text').getText())
+    course_info.append(html_tree.find(
+        'div',
+        class_='rc-Language').getText())
+    course_info.append(html_tree.find(
+        'div',
+        class_='startdate rc-StartDateString caption-text').getText())
+    rating = html_tree.find(
+        'div',
+        class_='ratings-text headline-2-text')
     if rating:
         course_info.append(rating.getText())
     else:
         course_info.append('None')
-    course_info.append(len(html_tree.findAll('div', class_='week')))
+    course_info.append(len(html_tree.findAll(
+            'div',
+            class_='week')))
     course_info.append(course_link)
     return course_info
 
@@ -49,4 +59,3 @@ if __name__ == '__main__':
         courses_info.append(get_course_info(course_link))
     filepath = 'courses_info.xlsx'
     output_courses_info_to_xlsx(filepath, courses_info)
-    
